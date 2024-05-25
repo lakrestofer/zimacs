d0b1862cc649579f3d53ef2b3ec16e12dc677f85
lakrestofer <lakrestofer@gmail.com>
2024-05-26 01:51:37 +0200

    start writing tests for numbers, need to handle invalid input better

--- lexer/src/testing/numbers.zig
+++ lexer/src/testing/numbers.zig
@@ -6,6 +6,33 @@ const Token = root.Token;
 const cmod = root.Lexer.cmod;
 const testLexer = @import("./utils.zig").testLexer;
 
-test "whaaaat" {
-    try std.testing.expect(false);
+test "infnan" {
+    try testLexer(
+        &([_]Token{
+            Token.init(cmod.NUMBER, 0, 6),
+            Token.init(cmod.NUMBER, 7, 13),
+            Token.init(cmod.NUMBER, 14, 20),
+            Token.init(cmod.NUMBER, 21, 27),
+        }),
+        "+inf.0 -inf.0 +nan.0 -nan.0",
+    );
+}
+
+test "binary" {
+    try testLexer(&([_]Token{Token.init(cmod.NUMBER, 0, 10)}), "#b01010101");
+    ok fortsätt här! vi behöver fixa ett bra sätt för invalid tokens att hanteras
+    try testLexer(&([_]Token{Token.init(cmod.INVALID, 0, 1)}), "#b02");
+}
+test "octal" {
+    try testLexer(&([_]Token{Token.init(cmod.NUMBER, 0, 10)}), "#o07070707");
+    // try testLexer(&([_]Token{Token.init(cmod.INVALID, 0, 10)}), "#o08080808");
+}
+
+test "decimal" {
+    try testLexer(
+        &([_]Token{
+            Token.init(cmod.NUMBER, 0, 10),
+        }),
+        "#o01010101",
+    );
 }
